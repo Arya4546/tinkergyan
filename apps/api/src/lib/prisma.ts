21cb28ad8956bb8318a6001df8b038a730a5c9b0
Arya Deep Singh <singharya9693@gmail.com>
2026-04-24 22:38:48 +0530

    fixed build issue

--- apps/api/src/lib/prisma.ts
+++ apps/api/src/lib/prisma.ts
@@ -1,5 +1,4 @@
-import pkg from '../../generated/client/index.js';
-const { PrismaClient } = pkg;
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as { prisma: any | undefined };
 
